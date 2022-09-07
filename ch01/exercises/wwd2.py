199fa234bb77f797fb48b0a46a7544045067b89f
Sangyoon Kwon <112511940+skwon23@users.noreply.github.com>
2022-09-07 00:19:45 +0000

    exercise

---
+++ ch01/exercises/wwd2.py
@@ -0,0 +1,17 @@
+print(10 * 5)
+print(10 ** 2)
+print(15 / 10)
+print(15 // 10)
+print(-15 // 10)
+print(15 % 10)
+print(10 % 15)
+print(10 % 10)
+print(0 % 10)
+print(10 / 15)
+#An infinite decimal
+
+rate = input ("Please enter Euro/dollar exchange rate.")
+amount = input ("please enter how much you will exchange.")
+total = float(rate)*float(amount)
+result = float(total) - 3
+print(float(result))
