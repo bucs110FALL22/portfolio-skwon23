7beb9cc3fc089b2e9079e11117f25b27ba18220d
Sangyoon Kwon <112511940+skwon23@users.noreply.github.com>
2022-09-08 01:57:13 +0000

    myturtle

---
+++ ch02/exercises/myturtle.py
@@ -0,0 +1,26 @@
+from turtle import *
+screensize(70,70)
+color('purple')
+shape("turtle")
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+
+color('red')
+up()
+goto(-50,-10)
+
+down()
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+
+exitonclick()
