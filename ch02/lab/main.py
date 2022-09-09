e24fd72c76b40a56a8e316167b3914619656d691
Sangyoon Kwon <112511940+skwon23@users.noreply.github.com>
2022-09-09 20:50:04 +0000

    ch2 lab

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,10 +1,18 @@
-
+import random
 #Part A
 weeks = 16
 classes = 5
 tuition = 6000
+
+classes_per_week = 3
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
 
+cost_per_class = int(cost_per_week) / int(classes_per_week)
+
+print("The cost per class is", float(cost_per_class))
 
 #Part B
+list = ['apple','banana','grape','watermelon', 'peach']
+random_fruit = random.choice(list)
+print(random_fruit)
